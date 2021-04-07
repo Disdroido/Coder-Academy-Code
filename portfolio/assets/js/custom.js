7f1a80ab7cf47ebe6bebcdf562f6242551dd17c2
Disdroido <disdroido@gmail.com>
2021-04-07 10:06:27 +1000

    changes to navbar and js

--- portfolio/assets/js/custom.js
+++ portfolio/assets/js/custom.js
@@ -1,57 +1,21 @@
-// $('.btn').click(function() {
-//     $('html, body').animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top-70
-//     }, 1000);
-//     return false;
-// });
-
-// $(function() {
-//     $(".btn").click(function() {
-//        // remove classes from all
-//        $(".btn").removeClass("active");
-//        // add class to the one we clicked
-//        $(this).addClass("active");
-//     });
-//  });
-
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-
-    //smoothscroll
-    $('.btn').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('.btn').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
+$('.btn').click(function() {
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top-70
+    }, 1000);
+    return false;
 });
 
-// Use Your Class or ID For Selection 
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('.navbar a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.navbar ul li a').removeClass("active");
-            currLink.addClass("active");
+$(document).ready(function() {
+    var hero = $('.hero').offset();
+    var about = $('section:nth-of-type(1)').offset();   
+    var $window = $(window);
+    
+    $window.scroll(function() {
+        if ( $window.scrollTop() >= hero.top) {
+            $(".link1").removeClass("active");
         }
-        else{
-            currLink.removeClass("active");
+        if ( $window.scrollTop() >= about.top) {
+            $(".link1").removeClass("active").addClass("active");
         }
-    });
-}
+    });			
+});
