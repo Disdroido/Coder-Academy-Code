f4a28c59275aa679f4a92decafeb8733ae0af534
Disdroido <disdroido@gmail.com>
2021-04-07 10:30:52 +1000

    changes to about section style

--- portfolio/assets/js/custom.js
+++ portfolio/assets/js/custom.js
@@ -7,15 +7,29 @@ $('.btn').click(function() {
 
 $(document).ready(function() {
     var hero = $('.hero').offset();
-    var about = $('section:nth-of-type(1)').offset();   
+    var about = $('section:nth-of-type(1)').offset();
+    var projects = $('section:nth-of-type(2)').offset();
+    var skills = $('section:nth-of-type(3)').offset();  
     var $window = $(window);
     
     $window.scroll(function() {
         if ( $window.scrollTop() >= hero.top) {
             $(".link1").removeClass("active");
         }
+        if ( $window.scrollTop() >= hero.top) {
+            $(".link2").removeClass("active");
+        }
+        if ( $window.scrollTop() >= hero.top) {
+            $(".link3").removeClass("active");
+        }
         if ( $window.scrollTop() >= about.top) {
             $(".link1").removeClass("active").addClass("active");
+        }
+        if ( $window.scrollTop() >= projects.top) {
+            $(".link2").removeClass("active").addClass("active");
+        }
+        if ( $window.scrollTop() >= skills.top) {
+            $(".link3").removeClass("active").addClass("active");
         }
     });			
 });
